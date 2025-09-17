27a574dbebe7a974fdd571e5384b918a170bee47
stumburs <stumburs@inbox.lv>
2025-09-17 06:22:02 +0300

    Add !commands

---
+++ bot/commands/get_docs.py
@@ -0,0 +1,25 @@
+from .base_command import BaseCommand, PermissionLevel
+from twitchAPI.chat import ChatCommand
+
+
+class GetDocsCommand(BaseCommand):
+    @property
+    def name(self) -> str:
+        return "commands"
+
+    @property
+    def description(self) -> str:
+        return f"Sends a link to this page."
+
+    @property
+    def usage(self) -> str:
+        return "!commands"
+
+    @property
+    def permissions(self) -> list[PermissionLevel]:
+        return [PermissionLevel.ALL]
+
+    async def execute(self, cmd: ChatCommand) -> None:
+        await cmd.reply(
+            "Use and abuse me with these commands: https://stumburs.github.io/pizza-son/commands/"
+        )
