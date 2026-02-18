c5778b812b73bb06761ee5ebc2c29f5aa3ac82f5
stumburs <stumburs@inbox.lv>
2026-02-19 01:22:30 +0200

    Added initial logging.

--- cmd/pizza-son/main.go
+++ cmd/pizza-son/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"pizza-son/internal/bot"
 	"pizza-son/internal/commands"
 	"pizza-son/internal/config"
@@ -11,14 +12,17 @@ import (
 
 func main() {
 
+	log.Println("Initializing Markov...")
 	generator := mgo.NewMarkovGenerator()
 	generator.ReadNgrams("data.bin")
 
+	log.Println("Loading config...")
 	cfg, err := config.Load("config.toml")
 	if err != nil {
 		panic(err)
 	}
 
+	log.Println("Creating client...")
 	client := twitch.NewClient(cfg.Twitch.User, cfg.Twitch.OAuth)
 
 	ctx := &commands.Context{
@@ -34,9 +38,14 @@ func main() {
 	})
 
 	client.OnPrivateMessage(router.HandleMessage)
+	client.OnConnect(func() {
+		log.Println("Connected!")
+	})
 
+	log.Println("Joining channel(s):", cfg.Bot.Channels)
 	client.Join(cfg.Bot.Channels...)
 
+	log.Println("Connecting...")
 	if err := client.Connect(); err != nil {
 		panic(err)
 	}
