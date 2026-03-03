c4323fb18c84be426067ffcc617fade78793008c
stumburs <stumburs@inbox.lv>
2026-03-03 23:18:37 +0200

    Add repeat command to echo user messages

---
+++ internal/commands/repeat.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"pizza-son/internal/bot"
+	"strings"
+)
+
+func init() {
+	Register(bot.Command{
+		Name:        "repeat",
+		Description: "Repeats what you tell it to. Pretty self explanatory.",
+		Usage:       "!repeat <text>",
+		Permission:  bot.BotModerator,
+		Handler: func(ctx bot.CommandContext) {
+			if len(ctx.Args) == 0 {
+				ctx.Client.Reply(ctx.Message.Channel, ctx.Message.ID, "Usage: !repeat <text>")
+				return
+			}
+			ctx.Client.Say(ctx.Message.Channel, strings.Join(ctx.Args, " "))
+		},
+	})
+}
