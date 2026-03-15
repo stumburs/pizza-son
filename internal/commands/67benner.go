a9f7adecccb009e66ecb3c1bdab3abc2b343d8f1
stumburs <stumburs@inbox.lv>
2026-03-15 23:18:57 +0200

    improve 67 filtering

--- internal/commands/67benner.go
+++ internal/commands/67benner.go
@@ -3,20 +3,23 @@ package commands
 import (
 	"pizza-son/internal/bot"
 	"pizza-son/internal/services"
+	"regexp"
 	"strings"
 )
+
+var sixSevenRegex = regexp.MustCompile(`(?i)(^|\s)67(\s|$)|sixty[-\s]?seven|six[-\s]?seven`)
 
 func init() {
 	RegisterListener(bot.ListenerEntry{
 		Name:        "67-benner",
 		Description: "Detects 67s in messages and ben's",
 		Handler: func(ctx bot.CommandContext) bool {
-			if strings.TrimSpace(ctx.Message.Message) == "67" {
-				ctx.Client.Say(ctx.Message.Channel, "ben")
-				services.TwitchServiceInstance.Timeout(ctx.Message.Channel, ctx.Message.User.ID, 69, "ben")
-				return true
+			if !sixSevenRegex.MatchString(strings.TrimSpace(ctx.Message.Message)) {
+				return false
 			}
-			return false
+			ctx.Client.Say(ctx.Message.Channel, "ben")
+			services.TwitchServiceInstance.Timeout(ctx.Message.Channel, ctx.Message.User.ID, 69, "ben")
+			return true
 		},
 	})
 }
