8474c03b3cfd942a5aa71069827f572f9fcb6e4e
stumburs <stumburs@inbox.lv>
2026-08-03 11:18:47 +0300

    update url regex

--- internal/commands/urllistener.go
+++ internal/commands/urllistener.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-var urlRegex = regexp.MustCompile(`(?i)(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)|www\.[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)|[-a-zA-Z0-9@:%._+~#=]{1,256}\.(com|net|org|io|tv|gg|dev|app|me|co|uk|de|ru|fr|ca|au|jp|live|stream|chat|games|cloud|tech|ai)([-a-zA-Z0-9()@:%_+.~#?&/=]*))`)
+var urlRegex = regexp.MustCompile(`(?i)(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)|www\.[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)|[-a-zA-Z0-9@:%._+~#=]{1,256}\.(com|net|org|io|tv|gg|dev|app|me|co|uk|de|ru|fr|ca|au|jp|lv|live|stream|chat|games|cloud|tech|ai)([-a-zA-Z0-9()@:%_+.~#?&/=]*))`)
 
 func init() {
 	RegisterListener(bot.ListenerEntry{
